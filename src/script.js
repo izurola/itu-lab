c29ee62bf8d1cf6f6c007986548cccd38f580c50
ariuna221 <63195700+ariuna221@users.noreply.github.com>
2023-10-19 20:24:01 +0800

    nothing,just trying something

--- src/script.js
+++ src/script.js
@@ -1,21 +1,31 @@
 function validateForm() {
-    var district = document.getElementById("district")?.value;
-    var committee= document.getElementById("committee")?.value;
-    var appartment = document.getElementById("appartment")?.value;
-    var room = document.getElementById("room")?.value;
+    var district = document.getElementById("district").value;
+    var committee = document.getElementById("committee").value;
+    var appartment = document.getElementById("appartment").value;
+    var room = document.getElementById("room").value;
 
-    if (appartment == "") {
-        alert("Bair must be filled out");
+    if (district === "") {
+        alert("District must be filled out");
         return false;
     }
 
+    if (committee === "") {
+        alert("Committee must be filled out");
+        return false;
+    }
 
-    if( room == "") {
-        alert("toot must be filled out");
+    if (appartment === "") {
+        alert("Apartment must be filled out");
+        return false;
+    }
+
+    if (room === "") {
+        alert("Room must be filled out");
         return false;
     }
     
     alert("You have successfully registered!");
-    
+    return true; // Allow the form to submit
 }
+
 
