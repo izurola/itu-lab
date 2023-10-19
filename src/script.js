5ba503d30e89be0a41b16e19ca5103dd580fb5d3
ariuna221 <63195700+ariuna221@users.noreply.github.com>
2023-10-19 20:15:15 +0800

    js script process
    process

--- src/script.js
+++ src/script.js
@@ -1,17 +1,21 @@
 function validateForm() {
+    var district = document.getElementById("district")?.value;
+    var committee= document.getElementById("committee")?.value;
     var appartment = document.getElementById("appartment")?.value;
     var room = document.getElementById("room")?.value;
-    
 
     if (appartment == "") {
         alert("Bair must be filled out");
         return false;
     }
 
+
     if( room == "") {
         alert("toot must be filled out");
         return false;
     }
+    
+    alert("You have successfully registered!");
     
 }
 
